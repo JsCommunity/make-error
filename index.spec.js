203afe63bea898fd9ae29970deefa53ab7e7bbd4
Julien Fontanet <julien.fontanet@isonoe.net>
2014-10-11 16:00:43 +0200

    Better stack test.

--- index.spec.js
+++ index.spec.js
@@ -11,6 +11,35 @@ var expect = require('chai').expect;
 
 //====================================================================
 
+function getCommonLastItems(arr1, arr2, butFirsts) {
+  var n = Math.min(arr1.length, arr2.length) - (butFirsts || 0);
+
+  return [
+    arr1.slice(-n),
+    arr2.slice(-n),
+  ];
+}
+
+function compareStacks(actual, expected) {
+  actual = splitLines(actual);
+  expected = splitLines(expected);
+
+  // We want to compare only the beginning (bottom) of the stack and
+  // we don't want to compare the first (from head) two items.
+  var tmp = getCommonLastItems(actual, expected, 2);
+  actual = tmp[0];
+  expected = tmp[1];
+
+  expect(actual).to.deep.equal(expected);
+}
+
+var NL_RE = /\r?\n/;
+function splitLines(str) {
+  return String(str).split(NL_RE);
+}
+
+//====================================================================
+
 it('creates a new error class', function () {
   var constructorCalled;
 
@@ -23,7 +52,7 @@ it('creates a new error class', function () {
   }
   makeError(MyError);
 
-  var e = new MyError('my error message');
+  var e = new MyError('my error message'); var stack = new Error().stack;
 
   expect(e).to.be.an.instanceof(Error);
   expect(e).to.be.an.instanceof(BaseError);
@@ -34,6 +63,7 @@ it('creates a new error class', function () {
   expect(e.name).to.equal('MyError');
   expect(e.message).to.equal('my error message');
   expect(e.stack).is.a.string;
+  compareStacks(e.stack, stack);
 });
 
 it('derives an existing error class', function () {
@@ -50,7 +80,7 @@ it('derives an existing error class', function () {
   }
   makeError(MyDerivedError, MyBaseError);
 
-  var e = new MyDerivedError('my error message');
+  var e = new MyDerivedError('my error message'); var stack = new Error().stack;
 
   expect(e).to.be.an.instanceof(Error);
   expect(e).to.be.an.instanceof(BaseError);
@@ -60,4 +90,5 @@ it('derives an existing error class', function () {
   expect(e.name).to.equal('MyDerivedError');
   expect(e.message).to.equal('my error message');
   expect(e.stack).is.a.string;
+  compareStacks(e.stack, stack);
 });
