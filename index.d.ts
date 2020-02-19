7d299158b0ad35d3fbc2d1798f31b401e115639d
Alex Charles <acharles@convoy.com>
2020-02-19 01:33:11 -0800

    fix(types/BaseError): extend, rather than implement Error (#25)

--- index.d.ts
+++ index.d.ts
@@ -24,7 +24,7 @@ declare namespace makeError {
   /**
    * Use with ES2015+ inheritance.
    */
-  export class BaseError implements Error {
+  export class BaseError extends Error {
     message: string;
     name: string;
     stack: string;
